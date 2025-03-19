78c4663872ac9168e719a34448a62fad6c67e15d
shaniii <shanraboy11@gmail.com>
2025-03-19 23:09:44 +0800

    added DisplayImage form

--- obj/Debug/net8.0-windows/NEXUS.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NEXUS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d0614a567d4d7484abde49ff2f9920fa83d7159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59cf2abcb2afbdb889bfa60c48ddcf0af6750e5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
