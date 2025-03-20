885c63a8af950a8091329e03f19a9501a6fe3cc9
shaniii <shanraboy11@gmail.com>
2025-03-20 23:58:07 +0800

    profile form added

--- Forms/Profile.Designer.cs
+++ Forms/Profile.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Profile";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Profile));
+            SuspendLayout();
+            // 
+            // Profile
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(230, 249, 255);
+            ClientSize = new Size(577, 667);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Profile";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Profile";
+            ResumeLayout(false);
         }
 
         #endregion
--- obj/Debug/net8.0-windows/NEXUS.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NEXUS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc51b1efc38fa89934946dac8569ef28205e4472")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f8d4c5e03f637390302b8b7fb88cd03eb75aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
