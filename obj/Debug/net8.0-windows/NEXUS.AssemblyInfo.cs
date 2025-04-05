ec3827204989dadc37bb040275d3007e3206c907
shaniii <shanraboy11@gmail.com>
2025-04-05 14:45:48 +0800

    added a folder for the jeep codes on the project's resources

--- obj/Debug/net8.0-windows/NEXUS.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NEXUS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a6e39c13da0dcd034af673eb97192e43e31429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae7ff3ae5d32921c54c182f148c474dd967f94bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NEXUS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
